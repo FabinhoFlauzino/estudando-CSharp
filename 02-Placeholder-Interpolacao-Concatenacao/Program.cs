3aaf3ce8d5530d94492aa4662a1eef47adc4406b
unknown <fabioflauzino@hotmail.com>
2024-04-14 12:42:56 -0300

    subindo arquivo 2

---
+++ 02-Placeholder-Interpolacao-Concatenacao/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace _02PlaceholderInterpolacaoeInterpolacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 30;
+            double saldo = 10.35783;
+            string nome = "Fulano de Tal";
+
+            //Placeholder muito usado antes da versão 6 do C#
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais.", nome, idade, saldo);
+
+            //Interpolação incluido na versão 6 do C#
+            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais.");
+
+            //Concatenação é o método mais antigo em relação aos outros dois
+            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("F2", CultureInfo.InvariantCulture) + " reais.");
+        }
+    }
+}
