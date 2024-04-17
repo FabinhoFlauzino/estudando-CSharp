24f19281c955b77e93b5e0ef1ddfe3a0201190b8
Fabio Flauzino <fabio@mfrural.com.br>
2024-04-17 15:02:04 -0300

    subindo arquivo 8

---
+++ 08-Exercicios-Propostos-Parte-1/Exercicio-05/Exercicio/Exercicio/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Globalization;
+
+namespace Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Fazer um programa para ler o código de uma peça 1, o número de peças 1, o valor unitário de cada peça 1, o
+            código de uma peça 2, o número de peças 2 e o valor unitário de cada peça 2. Calcule e mostre o valor a ser pago.
+            Exemplos:
+            Entrada:        Saída:
+            12 1 5.30
+            16 2 5.10
+                            VALOR A PAGAR: R$ 15.50
+            Entrada:        Saída:
+            13 2 15.30
+            161 4 5.20
+                            VALOR A PAGAR: R$ 51.40
+            Entrada:        Saída:
+            1 1 15.10
+            2 1 15.10
+                            VALOR A PAGAR: R$ 30.20
+            */
+
+            int cod1, cod2, qte1, qte2;
+            double preco1, preco2, total;
+
+            string[] valores = Console.ReadLine().Split(' ');
+            cod1 = int.Parse(valores[0]);
+            qte1 = int.Parse(valores[1]);
+            preco1 = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+            valores = Console.ReadLine().Split(' ');
+            cod2 = int.Parse(valores[0]);
+            qte2 = int.Parse(valores[1]);
+            preco2 = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+            total = preco1 * qte1 + preco2 * qte2;
+
+            Console.WriteLine("VALOR A PAGAR: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
