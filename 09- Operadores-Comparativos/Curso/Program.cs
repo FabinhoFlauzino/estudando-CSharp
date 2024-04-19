e33f861744e8ac311484ac1272c5db9e399e62a0
unknown <fabioflauzino@hotmail.com>
2024-04-18 22:43:38 -0300

    subindo arquivo 9

---
+++ 09- Operadores-Comparativos/Curso/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            bool c1 = a < 10;
+            bool c2 = a < 20;
+            bool c3 = a > 10;
+            bool c4 = a > 5;
+            Console.WriteLine(c1); //False
+            Console.WriteLine(c2); //True
+            Console.WriteLine(c3); //False  
+            Console.WriteLine(c4); //True
+            Console.WriteLine("------------");
+            bool c5 = a <= 10;
+            bool c6 = a >= 10;
+            bool c7 = a == 10;
+            bool c8 = a != 10;
+            Console.WriteLine(c5); //True
+            Console.WriteLine(c6); //True
+            Console.WriteLine(c7); //True
+            Console.WriteLine(c8); //False 
+        }
+    }
+}
