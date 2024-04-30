2253e6b9417ff86c6af50d02c56690b80754d6e1
Fabio Flauzino <fabio@mfrural.com.br>
2024-04-30 12:07:42 -0300

    subindo arquivo 12

---
+++ 12-Exercicios-Proposto-Parte-2-Estrutura-Condicional/Ecercicio-01/Exercicio/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Fazer um programa para ler um número inteiro, e depois dizer se este número é negativo ou não.
+            Exemplos:
+            Entrada:      Saída:
+            -10           NEGATIVO
+            Entrada:      Saída:
+            8             NAO NEGATIVO
+            Entrada:      Saída:
+            0             NAO NEGATIVO
+             */
+            Console.WriteLine("Digite um número: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero > 0)
+            {
+                Console.WriteLine($"NUMERO {numero}: NAO NEGATIVO");
+            } else
+            {
+                Console.WriteLine($"NUMERO {numero}: NEGATIVO");
+            }
+        }
+    }
+}
