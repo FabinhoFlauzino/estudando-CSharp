38754e99b1d9bbf265f7df16c2ddce471d76e701
Fabio Flauzino <fabio@mfrural.com.br>
2024-04-30 12:17:44 -0300

    subindo arquivo 12

---
+++ 12-Exercicios-Proposto-Parte-2-Estrutura-Condicional/Exercicio-02/Exercicio/Exercicio/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Exercicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Fazer um programa para ler um número inteiro e dizer se este número é par ou ímpar.
+                Exemplos:
+                Entrada:        Saída:
+                12              PAR
+                Entrada:        Saída:
+                -27             IMPAR
+                Entrada:        Saída:
+                0               PAR
+             
+             */
+
+            int numero = int.Parse(Console.ReadLine());
+
+            if(numero % 2 == 0)
+            {
+                Console.WriteLine("PAR");
+            } else
+            {
+                Console.WriteLine("IMPAR");
+            }
+        }
+    }
+}
