cb30fc28bebadf3f037b83f9e881660cb27ab52e
Fabio Flauzino <fabio@mfrural.com.br>
2024-04-30 12:13:37 -0300

    subindo arquivo 12

---
+++ 12-Exercicios-Proposto-Parte-2-Estrutura-Condicional/Exercicio-01/Exercicio/Exercicio/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            if (N < 0)
+            {
+                Console.WriteLine("NEGATIVO");
+            }
+            else
+            {
+                Console.WriteLine("NAO NEGATIVO");
+            }
+        }
+    }
+}
