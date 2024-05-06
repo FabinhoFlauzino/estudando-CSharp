a0fa99b377ab40474db23ead6d5db114abf51b95
Fabio Flauzino <fabio@mfrural.com.br>
2024-05-06 15:42:39 -0300

    subindo arquivo 17

---
+++ 17-Exercicios-For/Exercicio-02/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Exercicio_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, 
+            inclusive o X, se for o caso.
+             */
+
+            int X = int.Parse(Console.ReadLine());
+
+            if (X <= 1000)
+            {
+                for (int i = 1; i <= X; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+        }
+    }
+}
