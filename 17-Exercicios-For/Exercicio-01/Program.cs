c44a5a12412c3a8ab791d47310b3de97726a793e
Fabio Flauzino <fabio@mfrural.com.br>
2024-05-06 15:34:40 -0300

    subindo arquivo 17

---
+++ 17-Exercicios-For/Exercicio-01/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercicio_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Tabuada - Leia um valor inteiro N (2< N < 1000)
+                a seguir mostre a tabuada de N:
+                1XN ...
+             */
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int resultado = i * N;
+                Console.WriteLine($"{i} X {N} = {resultado}");
+            }
+        }
+    }
+}
