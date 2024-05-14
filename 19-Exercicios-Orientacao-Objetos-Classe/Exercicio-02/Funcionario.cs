52fa6b18dbd229a01f597f9afd096a96777f344b
Fabio Flauzino <fabio@mfrural.com.br>
2024-05-14 17:19:39 -0300

    subindo arquivo 19

---
+++ 19-Exercicios-Orientacao-Objetos-Classe/Exercicio-02/Funcionario.cs
@@ -0,0 +1,8 @@
+﻿namespace Exercicio_02
+{
+    class Funcionario
+    {
+        public string Nome;
+        public double Salario;
+    }
+}
---
+++ 19-Exercicios-Orientacao-Objetos-Classe/Exercicio-02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace Exercicio_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario1, funcionario2;
+            funcionario1 = new Funcionario();
+            funcionario2 = new Funcionario();
+
+            Console.WriteLine("Digite os dados do primeiro funcionário!");
+            Console.Write("Nome: ");
+            funcionario1.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            funcionario1.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Digite os dados do segundo funcionário!");
+            Console.Write("Nome: ");
+            funcionario2.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            funcionario2.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double salarioMedio = (funcionario1.Salario + funcionario2.Salario) / 2.0;
+            Console.WriteLine("Salário médio = " + salarioMedio.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
