71350fba7432a8594c88ece24c6c1df53d10f8b9
Fabio Flauzino <fabio@mfrural.com.br>
2024-05-15 17:49:14 -0300

    subindo arquivo 18

---
+++ 19-Exercicios-Orientacao-Objetos-Classe/metodo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+
+namespace metodo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Usando a classe criada
+            Triangulo x, y;
+            //Instanciando a Classe
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Entre com as medidas do triângulo X:");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Entre com as medidas do triângulo Y:");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //Usando o metodo da Classe
+            double areaX = x.Area();
+            double areaY = y.Area();
+
+            Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Maior área: X");
+            }
+            else
+            {
+                Console.WriteLine("Maior área: Y");
+            }
+        }
+    }
+}
---
+++ 19-Exercicios-Orientacao-Objetos-Classe/metodo/Triangulo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace metodo
+{
+    class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+
+        public double Area()
+        {
+            double p = (A + B + C) / 2.0;
+            //double raiz = Math.Sqrt(p * (p - A) * p * (p - B) * p * (p - C));
+            //return raiz;
+
+            //ou pode retornar direto sem usar variavel
+            return Math.Sqrt(p * (p - A) * p * (p - B) * p * (p - C));
+        }
+    }
+}
