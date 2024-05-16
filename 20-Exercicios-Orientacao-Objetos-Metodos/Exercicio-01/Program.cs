6d0dbb0db67cf47dec5a2daccea0e3d105a86ab2
Fabio Flauzino <fabio@mfrural.com.br>
2024-05-16 17:40:18 -0300

    subindo arquivo 20

---
+++ 20-Exercicios-Orientacao-Objetos-Metodos/Exercicio-01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace Exercicio_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+
+            Console.WriteLine("Entre a largura e a altura do retângulo: ");
+            retangulo.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            retangulo.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double area = retangulo.Area();
+            double perimetro = retangulo.Perimetro();
+            double diagonal = retangulo.Diagonal();
+
+            Console.WriteLine($"AREA = {area.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"PERÍMETRO = {perimetro.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"DIAGONAL = {diagonal.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
---
+++ 20-Exercicios-Orientacao-Objetos-Metodos/Exercicio-01/Retangulo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_01
+{
+    class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+
+        public double Perimetro()
+        {
+            return 2 * (Largura + Altura);
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt((Largura * Largura) + (Altura * Altura));
+        }
+    }
+}
