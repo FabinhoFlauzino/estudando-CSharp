8c6dffd5e162d777543f4ec0b179f5b6880de8d6
Fabio Flauzino <fabio@mfrural.com.br>
2024-05-17 09:07:11 -0300

    subindo arquivo 20

---
+++ 20-Exercicios-Orientacao-Objetos-Metodos/Exercicio-02/Funcionario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_02
+{
+    class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido ()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+
+        public void AumentarSalario(double aumentoPercentual)
+        {
+            double aumento = SalarioBruto * (aumentoPercentual / 100);
+            SalarioBruto += aumento;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome}, $ {SalarioLiquido()}";
+        }
+    }
+}
---
+++ 20-Exercicios-Orientacao-Objetos-Metodos/Exercicio-02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace Exercicio_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Salário Liquido de um funcionário");
+            Console.WriteLine();
+
+            Funcionario funcionario = new Funcionario();
+
+            Console.Write("Nome: ");
+            funcionario.Nome = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            funcionario.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Valor do Imposto: ");
+            funcionario.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+
+            Console.WriteLine($"Funcionário: {funcionario}");
+            Console.WriteLine();
+
+            Console.Write($"Digite a porcentagem para aumentar o salário do funcionario {funcionario.Nome}: ");
+            double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            funcionario.AumentarSalario(porcentagem);
+
+            Console.WriteLine($"Dados atualizados: {funcionario}");
+        }
+    }
+}
