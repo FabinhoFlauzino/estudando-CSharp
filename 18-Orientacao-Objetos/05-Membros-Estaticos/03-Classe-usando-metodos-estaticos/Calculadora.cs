a6a2e70304ac37d23979048b3794b052e8b7d317
Fabio Flauzino <fabio@mfrural.com.br>
2024-05-17 17:55:41 -0300

    subindo arquivo 18

---
+++ 18-Orientacao-Objetos/05-Membros-Estaticos/03-Classe-usando-metodos-estaticos/Calculadora.cs
@@ -0,0 +1,17 @@
+﻿namespace _03_Classe_usando_metodos_estaticos
+{
+    class Calculadora
+    {
+        public static double Pi = 3.14;
+
+        public static double Circunferencia(double r)
+        {
+            return 2.0 * Pi * r;
+        }
+
+        public static double Volume(double r)
+        {
+            return 4.0 / 3.0 * Pi * r * r * r;
+        }
+    }
+}
