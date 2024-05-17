4fec4e2c3fd5f1305874eaf2496e393842db1791
Fabio Flauzino <fabio@mfrural.com.br>
2024-05-17 17:53:14 -0300

    subindo arquivo 18

---
+++ 18-Orientacao-Objetos/05-Membros-Estaticos/02-Instanciando-objeto/Calculadora.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace _02_Instanciando_objeto
+{
+    class Calculadora
+    {
+        public double Pi = 3.14;
+
+        public double Circunferencia(double r)
+        {
+            return 2.0 * Pi * r;
+        }
+
+        public double Volume(double r)
+        {
+            return 4.0 / 3.0 * Pi * r * r * r;
+        }
+    }
+}
---
+++ 18-Orientacao-Objetos/05-Membros-Estaticos/02-Instanciando-objeto/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace _02_Instanciando_objeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calc = new Calculadora();
+            Console.Write("Entre o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double circ = calc.Circunferencia(raio);
+            double volume = calc.Volume(raio);
+            Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + calc.Pi.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
