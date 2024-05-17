052ea33e312c8946a8ee7ed97ec0179632a8ed9a
Fabio Flauzino <fabio@mfrural.com.br>
2024-05-17 12:00:02 -0300

    subindo arquivo 20

---
+++ 20-Exercicios-Orientacao-Objetos-Metodos/Exercicio-03/Aluno.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_03
+{
+    class Aluno
+    {
+        public string Nome;
+        public double PrimeiroTrimestre;
+        public double SegundoTrimestre;
+        public double TerceiroTrimestre;
+        int pontos = 60;
+        public double NotaFinal()
+        {
+            return (PrimeiroTrimestre + SegundoTrimestre + TerceiroTrimestre);
+        }
+
+        public string Situacao()
+        {
+            if (NotaFinal() >= pontos)
+            {
+                return "APROVADO";
+            }
+            else
+            {
+                double totalPontos = pontos - NotaFinal();
+
+                return $"REPROVADO \n" +
+                       $"FALTARAM {totalPontos.ToString("F2", CultureInfo.InvariantCulture)} PONTOS";
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"NOTA FINAL = {NotaFinal().ToString("F2", CultureInfo.InvariantCulture)}\n" +
+                   $"{Situacao()}";
+        }
+    }
+}
---
+++ 20-Exercicios-Orientacao-Objetos-Metodos/Exercicio-03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace Exercicio_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+
+            Console.Write("Nota do aluno: ");
+            aluno.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite as três notas do aluno: ");
+            aluno.PrimeiroTrimestre = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.SegundoTrimestre = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.TerceiroTrimestre = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(aluno);
+        }
+    }
+}
