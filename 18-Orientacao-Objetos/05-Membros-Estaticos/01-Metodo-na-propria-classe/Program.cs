6d0cd36ca0a9b55ede400f47557a7b486409825c
Fabio Flauzino <fabio@mfrural.com.br>
2024-05-17 17:51:00 -0300

    subindo arquivo 18

---
+++ 18-Orientacao-Objetos/05-Membros-Estaticos/01-Metodo-na-propria-classe/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace _01_Metodo_na_propria_classe
+{
+    class Program
+    {
+        static double Pi = 3.14;
+
+        static void Main(string[] args)
+        {
+            Console.Write("Entre o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double circ = Circunferencia(raio);
+            double volume = Volume(raio);
+            Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + Pi.ToString("F2", CultureInfo.InvariantCulture));
+        }
+
+        static double Circunferencia(double r)
+        {
+            return 2.0 * Pi * r;
+        }
+
+        static double Volume(double r)
+        {
+            return 4.0 / 3.0 * Pi * r * r * r;
+        }
+    }
+}
