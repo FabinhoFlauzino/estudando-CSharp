7229c0365fcc943e393bd8594c2d24eaa15eb2a2
unknown <fabioflauzino@hotmail.com>
2024-05-17 23:50:40 -0300

    subindo arquivo 21

---
+++ 21-Exercicios-Orientacao-Objetos-Metodos-Estaticos/ConversorDeMoeda.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21_Exercicios_Orientacao_Objetos_Metodos_Estaticos
+{
+    class ConversorDeMoeda
+    {
+        public static double Iof = 6.00;
+
+        public static double ConverterDolarParaReal(double cotacaoDolar, double valorComprado)
+        {
+            double porcentagem = Iof / 100;
+            double totalComprado = cotacaoDolar * valorComprado;
+
+            return totalComprado += (totalComprado * porcentagem);
+        }
+    }
+}
---
+++ 21-Exercicios-Orientacao-Objetos-Metodos-Estaticos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace _21_Exercicios_Orientacao_Objetos_Metodos_Estaticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual a cotação do dólar? ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            double qtdeDolaresComprado = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double valor = ConversorDeMoeda.ConverterDolarParaReal(cotacao, qtdeDolaresComprado);
+
+            Console.WriteLine($"Valor a ser pago em reais = {valor.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
