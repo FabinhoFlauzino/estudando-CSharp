30bcb7bc279f96973a72a9a85994778c7f6816d6
Fabio Flauzino <fabio@mfrural.com.br>
2024-05-27 12:11:12 -0300

    subindo arquivo 22

---
+++ 22-Construtor/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Globalization;
+
+namespace _22_Construtor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Entre os dados do produto:");
+
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantidade no estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Produto produto = new Produto(nome, preco, quantidade);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Dados do produto: " + produto);
+
+            Console.WriteLine();
+
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            produto.AdicionarProdutos(qte);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Dados atualizados: " + produto);
+
+            Console.WriteLine();
+
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            produto.RemoverProdutos(qte);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Dados atualizados: " + produto);
+        }
+    }
+}
