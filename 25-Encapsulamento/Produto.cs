729e3b3f267e5768f7662a13b4dad3ac0199d2c3
Fabio Flauzino <fabio@mfrural.com.br>
2024-06-14 16:19:12 -0300

    subindo arquivo 25

---
+++ 25-Encapsulamento/Produto.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25_Encapsulamento
+{
+    public class Produto
+    {
+        // Atributos privados
+        private string _nome;
+
+        // Propriedades autoimplementadas
+        public double Preco { get; private set; }
+        public double Quantidade { get; set; }
+
+        // Construtores
+        public Produto() { }
+
+        public Produto(string nome, double preco, double quantidade) // Corrigi a quantidade para double
+        {
+            _nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        // Propriedades customizadas usando propriedades privadas
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+        // Propriedade customizada usando propriedades autoimplementadas
+        public double ValorTotalEmEstoque
+        {
+            get { return Preco * Quantidade; }
+        }
+
+        // Método público para atualizar o preço
+        public void AtualizarPreco(double novoPreco)
+        {
+            if (novoPreco >= 0)
+            {
+                Preco = novoPreco;
+            }
+        }
+
+        // Métodos para adicionar e remover produtos do estoque
+        public void AdicionarProdutos(double quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(double quantidade)
+        {
+            if (Quantidade >= quantidade)
+            {
+                Quantidade -= quantidade;
+            }
+        }
+
+        // Sobrescreve o método ToString para fornecer uma representação da classe Produto
+        public override string ToString()
+        {
+            return _nome
+            + ", $ "
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ 25-Encapsulamento/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _25_Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Implementação Manual
+            //Produto p = new Produto("TV", 500.00, 10);
+            //p.SetNome("TV 4k");
+            //Console.WriteLine(p.GetNome());
+            //Console.WriteLine(p.GetPreco());
+            //Console.WriteLine(p.GetQuantidade());
+
+            // Cria uma instância de Produto usando o construtor padrão
+            Produto produto1 = new Produto();
+
+            // Define o nome, preço e quantidade usando as propriedades e métodos
+            produto1.Nome = "Caneta";
+            produto1.AtualizarPreco(1.50);
+            produto1.Quantidade = 100;
+
+            // Exibe o produto usando o método ToString
+            Console.WriteLine(produto1);
+
+            // Cria uma instância de Produto usando o construtor parametrizado
+            Produto produto2 = new Produto("Lápis", 0.80, 200);
+
+            // Exibe o produto usando o método ToString
+            Console.WriteLine(produto2);
+
+            // Adiciona 50 lápis ao estoque
+            produto2.AdicionarProdutos(50);
+
+            // Remove 30 lápis do estoque
+            produto2.RemoverProdutos(30);
+
+            // Exibe o produto após as alterações no estoque
+            Console.WriteLine(produto2);
+        }
+    }
+}
